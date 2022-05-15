e10eb4b6bb71254739ac7a4d8e5e0ca5a5799a46
kasparnoor <68809743+kasparnoor@users.noreply.github.com>
2022-05-15 20:22:04 +0300

    Pregenerated static files and removed mobile console and more stuff

--- trk/trk/settings.py
+++ trk/trk/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'tailwind',
+    "whitenoise.runserver_nostatic",
     'tunniplaan.apps.TunniplaanConfig',
     'django.contrib.admin',
     'django.contrib.auth',
